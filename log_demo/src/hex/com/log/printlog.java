0fbc9a00387e26465a8b78693c979ead16907afe
huoex <huoex0628@gmai.com>
2014-06-20 16:51:23 +0800

    log4j
    Signed-off-by: huoex <huoex0628@gmai.com>

--- log_demo/src/hex/com/log/printlog.java
+++ log_demo/src/hex/com/log/printlog.java
@@ -1,9 +1,25 @@
 package hex.com.log;
 
+import org.apache.log4j.Logger;
+
+
 public class printlog {
-	String aa;
-	String bb;
-	String cc;
-	String dd;
-	//
+	private static final Logger LOGGER = Logger.getLogger("hex.com.log.printlog");
+	public static void errlog(){
+		LOGGER.debug("BB");
+	}
+	
+	public static void infolog(){
+		LOGGER.info("info");
+	}
+	
+	public static void tt(){
+		if(LOGGER.isDebugEnabled()){
+			LOGGER.debug("bug！");
+		}
+		if(LOGGER.isInfoEnabled()){
+			LOGGER.info("info!");
+		}
+	}
+	
 }
---
+++ log_demo/src/hex/com/log/printlogTest.java
@@ -0,0 +1,18 @@
+package hex.com.log;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class printlogTest {
+
+	@Test
+	public void test() {
+//		printlog log = new printlog();
+//		printlog.errlog();
+//		printlog.infolog();
+		printlog.tt();
+//		fail("Not yet implemented");
+	}
+
+}
